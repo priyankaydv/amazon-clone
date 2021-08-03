2a6034e60d59c3b98ceb7b6e42ffd20656ebbad4
Priyanka Yadav <ydpriyanka01@gmail.com>
2021-08-03 20:45:47 +0530

    added slider

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Home.css';
 import Product from './Product';
+import Carousel from 'react-elastic-carousel';
 
 function Home() {
     return (
         <div className="home">
         <div className="home_container">
-        <img  className=" home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.JPG" alt=""/>
+        <br/>
+        <Carousel >
+        <img  className=" home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_1x._CB667161802_.jpg" alt=""/>
+        <img  className=" home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg" alt=""/>
+        <img className="home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg" alt="" />
+        <img className="home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" alt="" />
+        <img className="home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Toys_en_US_1x._CB431858161_.jpg" alt="" />
+
+
+        </Carousel>
 
         <div className="home_row">
         <Product  
